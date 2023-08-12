addd34cb89c32cea1121d05a3082810d2b667561
TempuraSoup <travis041112@gmail.com>
2023-08-12 01:21:50 -0700

    more funtionality

--- tipping.js
+++ tipping.js
@@ -2,16 +2,47 @@
 
 function calculateAmounts()
 {
+    var split = parseInt(document.getElementById("split").value);
     var cost = parseFloat(document.getElementById('cost').value);
     var customPer = parseFloat(document.getElementById('custom').value);
     var customAmount;
     var totalAmount;
+    var splitAmount;
 
     customPer /= 100;
     customAmount = cost*customPer;
     totalAmount = cost + customAmount;
 
+    if (isNaN(split))
+    {
+        split = 1;
+    }
+    splitAmount = totalAmount/split;
+
     document.getElementById("tip").textContent = "Tip Amount: " + customAmount;
     document.getElementById('total').textContent = "Total Amount (including tip): " + totalAmount;
+    document.getElementById("splitAmount").textContent = "Splited Amount: " + splitAmount;
 }
 
+function buttonAmount(per)
+{
+    var cost = parseFloat(document.getElementById('cost').value);
+    var split = parseInt(document.getElementById("split").value);
+    var tipAmount;
+    var totalAmount;
+    var splitAmount;
+
+    if (isNaN(split))
+    {
+        split = 1;
+    }
+
+    tipAmount = cost*per;
+    totalAmount = cost+tipAmount;
+    splitAmount = totalAmount/split;
+
+    document.getElementById("tip").textContent = "Tip Amount: " + tipAmount;
+    document.getElementById('total').textContent = "Total Amount (including tip): " + totalAmount;
+    document.getElementById("splitAmount").textContent = "Splited Amount: " + splitAmount;
+
+}
